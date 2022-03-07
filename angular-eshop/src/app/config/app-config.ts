18e18abd5881a433fe77e03d3128ca736fe28de1
Zhdzd <zahidabdullah18@gmail.com>
2022-03-07 17:19:55 +0800

    working deployment

--- angular-eshop/src/app/config/app-config.ts
+++ angular-eshop/src/app/config/app-config.ts
@@ -5,7 +5,7 @@ export default {
     clientId: '0oa405voqutEZegdm5d7',
     issuer: 'https://dev-06780521.okta.com/oauth2/default',
     //redirectUri: 'http://localhost:8080/login/callback',
-    redirectUri: 'https://vespa-hawker.herokuapp.com/login/callback',
+    redirectUri: 'https://vespa-online-hawker.herokuapp.com/login/callback',
 
     scopes: ['openid', 'profile', 'email']
   }
